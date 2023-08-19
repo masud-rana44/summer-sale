7384457a78d12ddf8780666b55f04a9e272b796e
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-20 01:30:14 +0600

    completed apply coupon and reset functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 const cards = document.querySelectorAll(".card");
-const cartItemContainer = document.getElementById("cart-item-container");
-const totalItemPriceEl = document.getElementById("total-price");
+const cartItemsContainer = document.getElementById("cart-item-container");
+const totalCartPriceEl = document.getElementById("total-price");
+const discountEl = document.getElementById("discount");
+const totalEl = document.getElementById("total");
+const inputCoupon = document.querySelector(".input-coupon");
+const btnCoupon = document.querySelector(".btn-coupon");
 
 let item = 1;
 
@@ -14,12 +18,42 @@ cards.forEach((card) =>
       e.target.closest(".card").querySelector(".item-price").textContent
     );
 
-    const totalItemsPrice =
-      parseFloat(totalItemPriceEl.textContent) + itemPrice;
-    totalItemPriceEl.innerHTML = `${totalItemsPrice}TK`;
+    const totalCartPrice = parseFloat(totalCartPriceEl.textContent) + itemPrice;
+    totalCartPriceEl.innerHTML = `${totalCartPrice.toFixed(2)}TK`;
+
+    if (totalCartPrice >= 200) btnCoupon.removeAttribute("disabled");
 
     const li = document.createElement("li");
     li.textContent = `${item++}. ${itemName}`;
-    cartItemContainer.appendChild(li);
+    cartItemsContainer.appendChild(li);
   })
 );
+
+btnCoupon.addEventListener("click", () => {
+  const totalCartPrice = parseFloat(totalCartPriceEl.textContent);
+
+  if (totalCartPrice < 200) return;
+
+  if (!inputCoupon.value || inputCoupon.value !== "SELL200") {
+    alert("Please provide a valid coupon");
+    return;
+  }
+
+  const discount = (totalCartPrice / 100) * 20;
+  const total = totalCartPrice - discount;
+
+  discountEl.innerHTML = `${discount.toFixed(2)}TK`;
+  totalEl.innerHTML = `${total.toFixed(2)}TK`;
+
+  // clear the input field
+  inputCoupon.value = "";
+});
+
+document.querySelector("#btn-reset").addEventListener("click", () => {
+  item = 1;
+  cartItemsContainer.textContent = "";
+  totalCartPriceEl.innerHTML =
+    discountEl.innerHTML =
+    totalEl.innerHTML =
+      "00.00 TK";
+});
