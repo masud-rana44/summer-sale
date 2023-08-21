ca2f570f389cb54f71d231418ebdb0f5b2fe927a
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-21 14:04:59 +0600

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -2,29 +2,27 @@ const cards = document.querySelectorAll(".card");
 const cartItemsContainer = document.getElementById("cart-item-container");
 const totalCartPriceEl = document.getElementById("total-price");
 const discountEl = document.getElementById("discount");
-const totalEl = document.getElementById("total");
+const totalEl = document.getElementById("total-after-discount");
 const inputCoupon = document.querySelector(".input-coupon");
 const btnCoupon = document.querySelector(".btn-coupon");
 const btnHome = document.getElementById("btn-home");
 const btnPurchase = document.getElementById("btn-purchase");
-
 const modal = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
 
 let item = 1;
+let totalCartPrice = 0;
 
 cards.forEach((card) =>
   card.addEventListener("click", (e) => {
-    const itemName = e.target
-      .closest(".card")
-      .querySelector(".item-name").textContent;
-
+    const currentItem = e.currentTarget;
+    const itemName = currentItem.querySelector(".item-name").textContent;
     const itemPrice = parseFloat(
-      e.target.closest(".card").querySelector(".item-price").textContent
+      currentItem.querySelector(".item-price").textContent
     );
 
-    const totalCartPrice = parseFloat(totalCartPriceEl.textContent) + itemPrice;
-    totalCartPriceEl.innerHTML = `${totalCartPrice.toFixed(2)}TK`;
+    totalCartPrice += itemPrice;
+    totalCartPriceEl.textContent = totalCartPrice.toFixed(2);
 
     if (totalCartPrice > 0) btnPurchase.removeAttribute("disabled");
     if (totalCartPrice >= 200) btnCoupon.removeAttribute("disabled");
@@ -36,28 +34,26 @@ cards.forEach((card) =>
 );
 
 btnCoupon.addEventListener("click", () => {
-  const totalCartPrice = parseFloat(totalCartPriceEl.textContent);
-
   if (totalCartPrice < 200) return;
 
-  if (!inputCoupon.value || inputCoupon.value !== "SELL200") {
+  const couponCode = inputCoupon.value.trim();
+
+  if (couponCode !== "SELL200") {
     alert("Invalid coupon! Please provide a valid coupon.");
     return;
   }
 
-  const discount = (totalCartPrice / 100) * 20;
-  const total = totalCartPrice - discount;
+  const discountAmount = (totalCartPrice / 100) * 20;
+  const totalAmount = totalCartPrice - discountAmount;
 
-  discountEl.innerHTML = `${discount.toFixed(2)}TK`;
-  totalEl.innerHTML = `${total.toFixed(2)}TK`;
+  discountEl.textContent = discountAmount.toFixed(2);
+  totalEl.textContent = totalAmount.toFixed(2);
 
   // clear the input field
   inputCoupon.value = "";
 });
 
 btnPurchase.addEventListener("click", () => {
-  const totalCartPrice = parseFloat(totalCartPriceEl.textContent);
-
   if (totalCartPrice <= 0) return;
 
   modal.classList.remove("remove");
@@ -66,11 +62,14 @@ btnPurchase.addEventListener("click", () => {
 
 btnHome.addEventListener("click", () => {
   item = 1;
-  cartItemsContainer.textContent = "";
-  totalCartPriceEl.innerHTML =
-    discountEl.innerHTML =
-    totalEl.innerHTML =
-      "00.00TK";
+  totalCartPrice = 0;
+  cartItemsContainer.innerHTML = "";
+  inputCoupon.value = "";
+
+  totalCartPriceEl.textContent =
+    discountEl.textContent =
+    totalEl.textContent =
+      "0.00";
 
   modal.classList.add("remove");
   overlay.classList.add("remove");
